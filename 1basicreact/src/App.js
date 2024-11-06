f4df6e870ac2962885a381e16c102e19049c0878
Atharva Sawleshwarkar <atharvams009@gmail.com>
2024-11-06 17:19:15 +0530

    Cleaning default project

---
+++ 1basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>This is first react project! Good luck Atharva!</h1>
+  );
+}
+
+export default App;
---
+++ 1vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+    <h1>Hello world! This is react app using vite</h1>
+  )
+}
+
+export default App
