5157e9fb08f2519cd00ca0efe849c8eb583f6877
Atharva Sawleshwarkar <atharvams009@gmail.com>
2024-11-08 17:33:54 +0530

    Hooks Complete

--- 2CounterProject_HooksBasics/src/App.jsx
+++ 2CounterProject_HooksBasics/src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let counter = 14;
+  // let counter = 14;
+  let [counter, setCounter ] = useState(0);
 
-  const addValue = ()=>{console.log(counter++)}
-  const removeValue = ()=>{console.log(counter--)}
+  const addValue = () => {
+    if( counter >=0 && counter < 20){
+      setCounter(++counter);
+    }
+  };
+  const removeValue = () => {
+    if(counter <= 0){
+      setCounter(0);
+    }else{
+      setCounter(--counter)
+    }
+  };
 
   return (
     <>
-     <h2>This is project to learn hooks in react using counter project</h2>
-     <h3>Counter value : {counter}</h3>
-     <button onClick={addValue}>Add Value</button>
-     <br/>
-     <br/>
-     <button onClick={removeValue}>Remove Value</button>
+      <h2>This is project to learn hooks in react using counter project</h2>
+      <h3>Counter value : {counter}</h3>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
