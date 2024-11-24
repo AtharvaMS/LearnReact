fa13ef3698bc2a179071c23fa7b7f1633af245a1
Atharva Sawleshwarkar <atharvams009@gmail.com>
2024-11-24 15:04:03 +0530

    Small change

--- 8ContextApi/contextDemo/src/App.jsx
+++ 8ContextApi/contextDemo/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import Login from "./components/login";
 import Profile from "./components/profile";
-import UserContextProvider  from "./context/userContextProvider";
+import UserContextProvider from './context/userContextProvider'
+
 
 function App() {
   return (
