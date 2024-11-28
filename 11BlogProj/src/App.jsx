713d3b6ff372afd0e95a57aeda4bb7144a8e692a
Atharva Sawleshwarkar <atharvams009@gmail.com>
2024-11-28 23:14:30 +0530

    Config added

--- 11BlogProj/src/App.jsx
+++ 11BlogProj/src/App.jsx
@@ -9,7 +9,11 @@ function App() {
   return (
     <>
       <h1 className="text-white bg-gray-500 text-4xl">Hello world!</h1>
-      {console.log(import.meta.env.VITE_APP_URL)}
+      {console.log(import.meta.env.VITE_APPWRITE_URL)}
+      {console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID)}
+      {console.log(import.meta.env.VITE_APPWRITE_DATABASE_ID)}
+      {console.log(import.meta.env.VITE_APPWRITE_COLLECTION_ID)}
+      {console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID)}
     </>
   );
 }
