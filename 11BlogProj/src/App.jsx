cc82d00e4a04c3b4119ccfff9b15319d8f9d550b
Atharva Sawleshwarkar <atharvams009@gmail.com>
2024-11-28 20:52:06 +0530

    Blog initialized

---
+++ 11BlogProj/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-white bg-gray-500 text-4xl">Hello world!</h1>
+      {console.log(import.meta.env.VITE_APP_URL)}
+    </>
+  );
+}
+
+export default App;
