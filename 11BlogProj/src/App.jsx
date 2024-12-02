6f9797dd381ca6348b2c43ca6672e1b142afa8d1
Atharva Sawleshwarkar <atharvams009@gmail.com>
2024-12-02 22:42:18 +0530

    redux config

--- 11BlogProj/src/App.jsx
+++ 11BlogProj/src/App.jsx
@@ -9,12 +9,6 @@ function App() {
   return (
     <>
       <h1 className="text-white bg-gray-500 text-4xl">Hello world!</h1>
-      {console.log(import.meta.env.VITE_APPWRITE_URL)}
-      {console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID)}
-      {console.log(import.meta.env.VITE_APPWRITE_DATABASE_ID)}
-      {console.log(import.meta.env.VITE_APPWRITE_COLLECTION_ID)}
-      {console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID)}
-      {console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID)}
     </>
   );
 }
---
+++ 11BlogProj/src/redux/reduxConfig.js
@@ -0,0 +1,4 @@
+
+import { configureStore } from 'react-redux'
+
+const store = configureStore()
